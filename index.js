f32ebae27f7436a40e978daf2c6aa9a503f65d6d
luren2 <1215750724@qq.com>
2023-04-10 22:46:48 +0800

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const program = require('commander');
+const helpOption = require('./lib/core/help');
+
+program.version(require('./package.json').version);
+
+// 可选信息
+helpOption();
+
+// 创建其他指令
+// createCommands();
+
+program.parse(process.argv);
