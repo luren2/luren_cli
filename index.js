482b6468bad7d4de976e99ed08d7868773c6a910
luren2 <1215750724@qq.com>
2023-04-11 11:04:39 +0800

    创建create项目命令

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
 const helpOption = require('./lib/core/help');
+const createCommands = require('./lib/core/create');
 
 program.version(require('./package.json').version);
 
@@ -8,6 +9,6 @@ program.version(require('./package.json').version);
 helpOption();
 
 // 创建其他指令
-// createCommands();
+createCommands();
 
 program.parse(process.argv);
